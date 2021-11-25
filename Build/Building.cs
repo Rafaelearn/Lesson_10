38587bd1fef99929a128743acb7a2b060e11708d
Рафаэль Халилулин <RFKhalilulin@stud.kpfu.ru>
2021-11-25 21:21:39 +0300

    Add project files.

---
+++ Build/Building.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Build
+{
+    public class Building
+    {
+        public static uint countBuilding = 0;
+        private uint id = 137742; // first ID
+        private float height;
+        private byte numberStoreys;
+        private ushort numberFlats;
+        private byte numberEntrance;
+        #region GetSet methods
+        public uint GetID()
+        {
+            return id;
+        }
+        public float GetHeight()
+        {
+            return height;
+        }
+        public void SetHeight(float height)
+        {
+            if (height < 3)
+            {
+                System.Console.WriteLine("Error. Heightof Buildings > 3m");
+            }
+            else
+            {
+                this.height = height;
+            }
+        }
+        public byte GetNummberStoreys()
+        {
+            return numberStoreys;
+        }
+        public void SetNumberStoreys(byte numberStoreys)
+        {
+            this.numberStoreys = numberStoreys;
+        }
+        public ushort GetNumberFlats()
+        {
+            return numberFlats;
+        }
+        public void SetNumberFlats(ushort numberFlats)
+        {
+            this.numberFlats = numberFlats;
+        }
+        public byte GetNumberEntrance()
+        {
+            return numberEntrance;
+        }
+        public void SetNumberEntrance(byte numberEntrance)
+        {
+            this.numberEntrance = numberEntrance;
+        }
+        #endregion
+
+        public float GetHeightOfStory()
+        {
+            return height / numberStoreys;
+        }
+        public int GetAverageCountFlatsInEntrance()
+        {
+            return numberFlats / numberEntrance;
+        }
+        public int GetAverageCountFlatsOnStorey()
+        {
+            return numberFlats / numberStoreys;
+        }
+        internal Building()
+        {
+            id += countBuilding;
+            countBuilding++;
+        }
+        internal Building(float height, byte numberStoreys)
+        {
+            this.height = height;
+            this.numberEntrance = 1;
+            this.numberStoreys = numberStoreys;
+            this.numberFlats = numberStoreys;
+        }
+        internal Building(float height, byte numberStoreys, byte numberEntrance, ushort numberFlats) : this()
+        {
+            this.height = height;
+            this.numberEntrance = numberEntrance;
+            this.numberStoreys = numberStoreys;
+            this.numberFlats = numberFlats;
+        }
+        public void Display()
+        {
+            Console.WriteLine("ID: " + GetID());
+            Console.WriteLine("Height: " + GetHeight());
+            Console.WriteLine("Entrance: " + GetNumberEntrance());
+            Console.WriteLine("Flats: " + GetNumberFlats());
+            Console.WriteLine("Storeys: " + GetNummberStoreys());
+        }
+    }
+}
---
+++ Commerce/Bank.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Commerce
+{
+    public class Bank
+    {
+        private Hashtable hashtable = new Hashtable();
+        public ulong CreateAccount()
+        {
+            Account opened = new Account();
+            hashtable[opened.Number] = opened;
+            return opened.Number;
+        }
+        public ulong CreateAccount(TypeAccount type)
+        {
+            Account opened = new Account(type);
+            hashtable[opened.Number] = opened;
+            return opened.Number;
+        }
+
+        public void DeleteAccount(ulong number)
+        {
+            hashtable.Remove(number);
+        }
+        public List<Account> GetListAccount()
+        {
+            List<Account> accounts = new List<Account>();
+            foreach (var item in hashtable.Values)
+            {
+                if (item is Account)
+                {
+                    accounts.Add((Account)item);
+                }
+            }
+            return accounts;
+        }
+    }
+}
